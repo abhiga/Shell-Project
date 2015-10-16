3424a22a25e1eb6189c33fc4a8e8fea559317d70
abhiga <agaurav@purdue.edu>
2015-10-16 01:36:13 -0400

    Commit Shell

--- command.cc
+++ command.cc
@@ -248,7 +248,7 @@ int yyparse(void);
 extern "C" void avoid_controlc( int sig )
 {	
 	Command::_currentCommand.clear();
-	Command::_currentCommand.prompt();
+	//Command::_currentCommand.prompt();
 }
 
 main()
